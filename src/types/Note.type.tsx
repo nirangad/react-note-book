fbcbfd86aea440f593aecde543fc89d9b7064c80
Niranga De Silva <NirangaD@99x.lk>
2022-11-19 23:55:23 +0530

    Basic UI changes

---
+++ src/types/Note.type.tsx
@@ -0,0 +1,15 @@
+export type NoteFormProps = {
+  onSubmit: (data: Note) => void;
+};
+
+export type Note = {
+  id?: string;
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
