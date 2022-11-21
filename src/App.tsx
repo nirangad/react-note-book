5c212610ca30b8aae5b64a47a02f74301098556f
Niranga De Silva <NirangaD@99x.lk>
2022-11-21 23:25:58 +0530

    Notes updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
+import { useNavigate } from "react-router-dom";
 import NewNote from "./components/NewNote";
-import { Note, Tag } from "./types/Note.type";
+import { Note } from "./types/Note.type";
 
 import useLocalStorage, {
   StorageKey,
   StorageKeys,
 } from "./hooks/useLocalStorage";
+import Home from "./components/Home";
 
 function App() {
   const [notes, setNotes] = useLocalStorage<Note[]>(
@@ -18,20 +21,26 @@ function App() {
     []
   );
 
+  const navigate = useNavigate();
+
   const onCreateNote = (note: Note) => {
     note.id = uuidv4();
     setNotes((prevNotes) => {
       return [...prevNotes, note];
     });
 
-    return notes;
+    navigate(`/`);
   };
+
+  useEffect(() => {
+    console.log("Notes: ", notes);
+  }, [notes]);
 
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote onCreateNote={onCreateNote}/>} />
+        <Route path="/" element={<Home notes={notes} />} />
+        <Route path="/new" element={<NewNote onCreateNote={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>}></Route>
           <Route path="edit" element={<h1>Edit</h1>}></Route>
